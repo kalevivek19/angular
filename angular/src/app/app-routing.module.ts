f62c52dbc0b3a94696b4ea3c57fa1c399ee34939
Vivek Kale <kalevivek19@gmail.com>
2020-05-27 15:40:03 +0530

    Implemented Routing and Added Home, About Us, Contact Us Components

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { AboutusComponent } from "./aboutus/aboutus.component";
+import { ContactusComponent } from "./contactus/contactus.component";
 
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutusComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactusComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
