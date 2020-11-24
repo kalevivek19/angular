523cce35cd8a6cfd83603efbe3d8c980bb2930f6
vivekkale <kalevivek19@gmail.com>
2020-11-24 23:39:30 +0530

    - Minor changes.

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-const compression = require('compression');
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
 
-// Gzip
-app.use(compression());
+// Serve static files....
+app.use(express.static(__dirname + '/dist/angular'));
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-app.listen(port);
-
-// For all GET requests, send back index.html so that PathLocationStrategy can be used
+// Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular/index.html'));
 });
 
-console.log(`Server listening on ${port}`);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
